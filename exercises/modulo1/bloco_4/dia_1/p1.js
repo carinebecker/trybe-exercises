f6cc66949f659d58a7d06a4e617fccb68462d1e1
Carine Becker <caarine.m@gmail.com>
2020-11-25 18:15:05 -0300

    p1 e p2

---
+++ exercises/modulo1/bloco_4/dia_1/p1.js
@@ -0,0 +1,17 @@
+let a = 15;
+let b = 3;
+
+let soma = a + b;
+console.log('Resultado da soma: ' + soma);
+
+let subtrai = a - b;
+console.log('Resultado da subtração: ' + subtrai);
+
+let multiplica = a * b;
+console.log('Resultado da multiplicação: ' + multiplica);
+
+let divide = a / b;
+console.log('Resultado da divisão: ' + divide);
+
+let modulo = a % b;
+console.log('Resultado do módulo: ' + modulo);
