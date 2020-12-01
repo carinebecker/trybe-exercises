dd3575474c78de77c2eed6a77fe0339e66167a48
Carine Becker <caarine.m@gmail.com>
2020-11-30 22:44:48 -0300

    ex/4.3

---
+++ exercises/modulo1/bloco_4/dia_3/ex_3.js
@@ -0,0 +1,12 @@
+let n = 5;
+
+let coluna = '';
+
+
+for (let index = 0; index < n; index += 1) {
+    for (let j = 0; j < n; j += 1) {
+        coluna += ' '
+    } 
+    coluna += '*'
+    console.log(coluna)
+} 
