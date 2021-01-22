a356416c14775b35e40072a1a06f3c3c5708f55a
Carine Becker <caarine.m@gmail.com>
2021-01-22 16:30:14 -0300

    ajusta ex 2

--- exercises/modulo1/bloco_8/dia_1/exercise2.js
+++ exercises/modulo1/bloco_8/dia_1/exercise2.js
@@ -63,15 +63,13 @@ const books = [
   },
 ];
 
-function smallerName() {
+const smallerName = () => {
   let nameBook;
-  // escreva aqui o seu código
   books.forEach((book) => {    
     if (!nameBook || book.name.length < nameBook.length) {
       nameBook = book.name;
     }
   });
-  // Variável nameBook que receberá o valor do menor nome;
   return nameBook;
 }
 
