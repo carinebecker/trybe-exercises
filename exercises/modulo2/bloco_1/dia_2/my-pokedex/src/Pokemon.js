f8e474b70d2c9bd9fbb57b6b1a0e36934d5757db
Carine Becker <caarine.m@gmail.com>
2021-02-12 15:08:42 -0300

    refatora pokemon.js

--- exercises/modulo2/bloco_1/dia_2/my-pokedex/src/Pokemon.js
+++ exercises/modulo2/bloco_1/dia_2/my-pokedex/src/Pokemon.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 class Pokemon extends React.Component {
   render() {
-    const { name, type, averageWeight: {value: weight}, averageWeight: {measurementUnit: unit}, image } = this.props.pokemon
+    const { name, type, averageWeight: {value}, averageWeight: {measurementUnit: unit}, image } = this.props.pokemon
   
     return (
       <div className="card">
         <div className="card-info">
           <p>{name}</p>
           <p>{type}</p>
-          <p>Average weight: {weight}{unit}</p>
+          <p>Average weight: {value}{unit}</p>
         </div>
         <img src={image} alt={name} />
       </div>
