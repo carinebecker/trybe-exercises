36e38b4ff435c1106a8cb25db9e467d40331457a
Carine Becker <caarine.m@gmail.com>
2021-03-01 14:47:00 -0300

    pokemon

--- exercises/modulo2/bloco_11/dia_2/my-pokedex/src/Pokedex.js
+++ exercises/modulo2/bloco_11/dia_2/my-pokedex/src/Pokedex.js
@@ -2,10 +2,17 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component{
+  
+
   render() {
+
+    
     return (
-      <div className="cards">
-      {this.props.data.map((currentPokemon, index) => <Pokemon pokemon={currentPokemon} key={index} />)}
+      <div>
+        <div className="cards">
+          {this.props.data.map((currentPokemon, index) => <Pokemon pokemon={currentPokemon} key={index} />)}
+        </div>
+          <button>Próximo Pokémon</button>
       </div>
     )
   }
